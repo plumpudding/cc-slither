e743a25bbe8dc9bfa6536001a3488bcaf1a26803
Tanonic <noprocraft@gmail.com>
2016-08-10 15:31:09 +0200

    added classes for Items

--- src/at/computercamp/slither/server/SlowItem.java
+++ src/at/computercamp/slither/server/SlowItem.java
@@ -1,11 +1,21 @@
 package at.computercamp.slither.server;
 
+import java.time.Duration;
+import java.time.Instant;
+
 /**
  * Created by feliumla16 on 10.08.2016.
  */
 public class SlowItem extends Item {
 
-    public void Slow(){
 
+    @Override
+    protected void applyEffectToSnake(Snake snake) {
+        long startTime = System.currentTimeMillis();
+        snake.setIsSlow(true);
+        if (startTime >= 10000) {
+            snake.setIsSlow(false);
+            }
+        }
     }
 }

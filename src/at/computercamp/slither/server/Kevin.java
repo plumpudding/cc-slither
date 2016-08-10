5d5b328bfac3695d8e88d4d89356cbe65f9365cd
milapaess16 <milapaess16@CAMP37.computercamp.local>
2016-08-10 15:20:10 +0200

    add basic collision checking
    and change the move code a bit

--- src/at/computercamp/slither/server/Kevin.java
+++ src/at/computercamp/slither/server/Kevin.java
@@ -7,8 +7,13 @@ public class Kevin { //the game controller
 	//tick tick tick...
 	
 	public void tick() {
-		for (Snake snake:gs.snakes)
+		for (Snake snake:gs.snakes) {
 			snake.tick();
+			if (snake.isDead) {
+				gs.snakes.remove(snake);
+				snake = null;
+			}
+		}
 		
 		for (Item item:gs.inactiveItems) {
 			item.tick();
@@ -17,19 +22,79 @@ public class Kevin { //the game controller
 				item = null;
 			}
 		}
+		
+		//spawn items
+		
 	}
 
 	public GameState getGameState() {
 		return gs;
 	}
 
+	public GameObject getObjectAtPoint(Point point) {
+
+		for (GameObject obj:GameServer.getInstance().getController().getGameState().inactiveItems)
+			if (obj.isAtPoint(point))
+				return obj;
+		
+		for (GameObject obj:GameServer.getInstance().getController().getGameState().snakes)
+			if (obj.isAtPoint(point))
+				return obj;
+		
+		return null;
+	}
+	
 	//TODO do more stuff here
 	public void handleClientAction(ClientAction action, Snake snake) {
 		snake.setDirection(action.getDirection());
 		snake.setIsFast(action.isFast());
 	}
 	
+	public Point findEmptyPosition() {
+		Point p = null;
+		
+		//TODO prevent endless loop
+		while (true) {
+			int x = (int) (Math.random() * gs.boardLength);
+			int y = (int) (Math.random() * gs.boardHeight);
+			p = new Point(x, y);
+			if (getObjectAtPoint(p) == null) {
+				return p;
+			}
+		}
+	}
+	
+	public Point findEmptyPosition(Point nextTo) {
+		Point p = null;
+		
+		p = new Point(nextTo.x, nextTo.y + 1);
+		if(getObjectAtPoint(p) == null)
+			return p;
+			
+		p = new Point(nextTo.x, nextTo.y - 1);
+		if(getObjectAtPoint(p) == null)
+			return p;
+			
+		p = new Point(nextTo.x + 1, nextTo.y);
+		if(getObjectAtPoint(p) == null)
+			return p;
+			
+		p = new Point(nextTo.x - 1, nextTo.y);
+		if(getObjectAtPoint(p) == null)
+			return p;
+		
+		return null;
+	}
+	
 	public Snake addSnake(String name) {
+		Point[] startTiles = new Point[3];
+		
+		startTiles[0] = findEmptyPosition();
+		
+		for (int i = 1; i < startTiles.length; i++) {
+			startTiles[i] = findEmptyPosition(startTiles[i-1]);
+		}
+		
 		Snake snake = new Snake(name);
 		gs.snakes.add(snake);
 		return snake;
--- src/at/computercamp/slither/server/Snake.java
+++ src/at/computercamp/slither/server/Snake.java
@@ -17,99 +17,95 @@ public class Snake implements GameObject {
 	private Point oldHead;
 	private Point tileForHead;
 	public boolean isDead = false;
- 
+
 	@Override
 	public boolean isAtPoint(Point p) {
-		for (Point tile:tiles)
+		for (Point tile : tiles)
 			if (tile == p)
 				return true;
-		
+
 		return false;
 	}
 
 	public void move() {
 		oldHead = tiles.get(0);
 		tileForHead = tiles.get(1);
-		
-		newPoint = new Point();
-		switch(direction){
-			case NORTH:
-				newPoint = checkDirection(getNorthPoint(), getSouthPoint());
-				break;
-				
-			case EAST:
-				newPoint = checkDirection(getEastPoint(), getWesthPoint());
-				break;
-			
-			case SOUTH:
-				newPoint = checkDirection(getSouthPoint(), getNorthPoint());
-				break;
-			
-			case WEST:
-				newPoint = checkDirection(getWesthPoint(), getEastPoint());
-				break;
+
+		switch (direction) {
+		case NORTH:
+			newPoint = checkDirection(getNorthPoint(), getSouthPoint());
+			break;
+
+		case EAST:
+			newPoint = checkDirection(getEastPoint(), getWestPoint());
+			break;
+
+		case SOUTH:
+			newPoint = checkDirection(getSouthPoint(), getNorthPoint());
+			break;
+
+		case WEST:
+			newPoint = checkDirection(getWestPoint(), getEastPoint());
+			break;
 		}
-		
-		tiles.remove(tiles.size() - 1);
-		tiles.add(0, newPoint);
-		
+
+		GameObject collider = GameServer.getInstance().getController().getObjectAtPoint(newPoint);
+
+		if (collider != null)
+			collider.collide(this);
+		else {
+			tiles.remove(tiles.size() - 1);
+			tiles.add(0, newPoint);
+		}
+
 	}
-	
-	private Point checkDirection(Point newDirection, Point oppositeDirection){
-		if(newDirection == tileForHead){
+
+	private Point checkDirection(Point newDirection, Point oppositeDirection) {
+		if (newDirection == tileForHead) {
 			newPoint = oppositeDirection;
-		}else{
+		} else {
 			newPoint = newDirection;
 		}
 		return newPoint;
 	}
-	
-	private Point getNorthPoint(){
-		newPoint.y = oldHead.y - 1;
-		newPoint.x = oldHead.x;
-		return newPoint;
-		
+
+	private Point getNorthPoint() {
+		return new Point(oldHead.x, oldHead.y - 1);
 	}
-	private Point getEastPoint(){
-		newPoint.y = oldHead.y;
-		newPoint.x = oldHead.x + 1;
-		return newPoint;
-		
+
+	private Point getEastPoint() {
+		return new Point(oldHead.x + 1, oldHead.y);
 	}
-	private Point getSouthPoint(){
-		newPoint.y = oldHead.y + 1;
-		newPoint.x = oldHead.x;
-		return newPoint;
-		
+
+	private Point getSouthPoint() {
+		return new Point(oldHead.x, oldHead.y + 1);
 	}
-	private Point getWesthPoint(){
-		newPoint.y = oldHead.y;
-		newPoint.x = oldHead.x - 1;
-		return newPoint;
-		
+
+	private Point getWestPoint() {
+		return new Point(oldHead.x - 1, oldHead.y);
 	}
-	
+
 	public Snake(String name) {
 		this.name = name;
 	}
-	
+
 	@Override
 	public void tick() {
-		for (Item item:activeItems)
+		for (Item item : activeItems)
 			item.tick();
-		
+
 		tickCounter += 1;
 		// 1 tick = 100ms
 
-		if (isFast && tickCounter >= moveIntervalFast){
+		if (isFast && tickCounter >= moveIntervalFast) {
 			move();
 			tickCounter = 0;
-		} else if (tickCounter >= moveInterval){
+		} else if (tickCounter >= moveInterval) {
 			move();
 			tickCounter = 0;
 		}
 	}
-	
+
 	public String getName() {
 		return name;
 	}
@@ -117,14 +113,27 @@ public class Snake implements GameObject {
 	public void setDirection(Direction direction) {
 		this.direction = direction;
 	}
-	
+
 	public void setIsFast(boolean isFast) {
 		this.isFast = isFast;
 	}
 
 	@Override
 	public void collide(Snake s) {
-		//TODO
+		s.die();
 	}
-	
+
+	public void die() {
+		this.isDead = true;
+	}
+
+	public void addItem(Item item) {
+		activeItems.add(item);
+		item.activate();
+	}
+
+	public void addTile(Point tile) {
+		tiles.add(tile);
+	}
+
 }

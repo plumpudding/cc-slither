bb1ee485bd7c34c2095f416f4338939303fa7bc7
Laurids Hieden <laurhied16@computercamp.local>
2016-08-10 16:10:42 +0200

    client timeout

--- src/at/computercamp/slither/server/Client.java
+++ src/at/computercamp/slither/server/Client.java
@@ -7,22 +7,36 @@ public class Client {
 	private InetAddress address;
 	private int port;
 	private Snake snake;
-	
+	private long lastSeen;
+
 	public Client(InetAddress address, int port, Snake snake) {
 		this.address = address;
 		this.port = port;
 		this.snake = snake;
+		this.lastSeen = System.currentTimeMillis();
 	}
-	
+
 	public InetAddress getAddress() {
 		return address;
 	}
-	
+
 	public int getPort() {
 		return port;
 	}
-	
+
 	public Snake getSnake() {
 		return snake;
+	}
+
+	public void setLastSeen() {
+		lastSeen = System.currentTimeMillis();
+	}
+
+	public boolean isOnline() {
+		if (lastSeen + 20000 < System.currentTimeMillis()) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 }
